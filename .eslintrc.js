94a72945d108d8aa8bfaa4ff4db421ff1316de56
zhuangzhuang <zhuangzhuang@xiaoshijie.com>
2022-02-20 19:15:24 +0800

    fix

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:vue/essential",
+        "prettier",
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "vue"
+    ],
+    "rules": {
+    }
+}
